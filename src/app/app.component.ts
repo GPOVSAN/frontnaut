b6837c2ab7bacbec845527fec6ac7af5779a1b4d
GPOVSAN <andrespoveda20@gmail.com>
2022-01-25 20:03:49 -0500

    agregar año y eliminar boton compartir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,25 @@ export class AppComponent {
   cards = [
     {
       num: '1',
-      title: 'Pelicula  1',
+      title: 'Dead Pool',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      ano: '2014',
       buttonText: 'Button',
       img: './../assets/43439.jpg'
     },
     {
       num: '2',
-      title: 'Pelicula 2',
+      title: 'Guardianes De La Galaxia',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      ano: '2016',
       buttonText: 'Button',
-      img: './../assets/43439.jpg'
+      img: './../assets/galaxy.jpg'
     },
     {
       num: '3',
       title: 'Pelicula 3',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      ano: '2020',
       buttonText: 'Button',
       img: './../assets/43439.jpg'
     },
@@ -34,6 +37,7 @@ export class AppComponent {
       num: '4',
       title: 'Pelicula 4',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      ano: '2020',
       buttonText: 'Button',
       img: './../assets/43439.jpg'
     },
@@ -41,6 +45,7 @@ export class AppComponent {
       num: '5',
       title: 'Pelicula 5',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      ano: '2020',
       buttonText: 'Button',
       img: './../assets/43439.jpg'
     },
